bb64eba297de47fe60f4abb7eb03713981f46851
Jamie Falconer <mrfluffyoven@gmail.com>
2025-05-07 08:57:41 +0100

    add orangefox flags

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,11 @@
-# Recovery Info
-  export FOX_VERSION="A057G"
+# Orangefox Flags
+export ALLOW_MISSING_DEPENDANCIES=true
+export TARGET_ARCH=arm64
+export OF_FLASHLIGHT_ENABLE=1
+export OF_FL_PATH1="/sys/devices/virtual/camera/flash/rear_flash"
+export FOX_VERSION="Stable 1"
+export OF_MAINTAINER="SavedByLight"
+export TARGET_DEVICE_ALT="a05s"
+export OF_ENABLE_LPTOOLS=1
+export OF_USE_LEGACY_BATTERY_SERVICES=1
+#export OF_SCREEN_H=1800
